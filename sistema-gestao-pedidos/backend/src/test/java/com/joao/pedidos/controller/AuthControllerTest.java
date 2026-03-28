3de6cdf648615f7f8344f1c22881b07d598fdd5a
João Victor Santos Dumont <dumontjv@outlook.com>
2026-03-28 16:50:42 -0300

    Add auth controller test skeleton for order system

---
+++ sistema-gestao-pedidos/backend/src/test/java/com/joao/pedidos/controller/AuthControllerTest.java
@@ -0,0 +1,16 @@
+package com.joao.pedidos.controller;
+
+import org.junit.jupiter.api.Test;
+
+class AuthControllerTest {
+
+    @Test
+    void shouldAuthenticateWithDemoCredentials() {
+        // TODO: implementar teste com MockMvc para validar login demo
+    }
+
+    @Test
+    void shouldRejectInvalidCredentials() {
+        // TODO: implementar teste com MockMvc para rejeitar credenciais invalidas
+    }
+}
